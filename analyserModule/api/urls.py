d3660f069a240ea185dc7a426e1f0a2e5497fdbd
Ujjwal R <ujjwal.raj.5798@gmail.com>
2023-03-18 21:21:30 +0530

    api modification

--- analyserModule/api/urls.py
+++ analyserModule/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('',views.bank_analysis),
-    path('',views.bank_statement_analysis),
+    path('analyse/api/bank-analysis',views.bank_analysis),
+    path('analyse/api/BankStatementAnalysis',views.bank_statement_analysis),
     path('',views.gross_summary),
     path('',views.bank_name),
 ]
