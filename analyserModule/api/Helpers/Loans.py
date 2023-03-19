f56e3f61ab774b1e9520e286090d0ffbfc141aaf
Ujjwal R <ujjwal.raj.5798@gmail.com>
2023-03-19 16:26:19 +0530

    load info added

---
+++ analyserModule/api/Helpers/Loans.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+def getLoanInfo( start_date, end_date,bank_statement):
+  # Initialize dict
+  keywords = ['LOAN', 'Loan', 'loan', 'LN', 'ln']
+  monthly_loan={}     # dict: {key -> (debit, credit loan)}
+  currStr = start_date.split('-')
+  currYr = int(currStr[0])
+  currMnth = int(currStr[1])
+  endStr = end_date.split('-')
+  endYr = int(endStr[0])
+  endMnth = int(endStr[1])
+  mnth=start_date[:]
+
+  while not (currMnth == endMnth and currYr== endYr):
+    if(currMnth < 10):
+      currMnthStr = "0" + ("% s" % currMnth)
+    else:
+      currMnthStr = ("% s" % currMnth)
+    currStr = ("% s" % currYr) + "-" + currMnthStr
+    monthly_loan[currStr]=[0, 0]    # debit, credit
+      
+    if (currMnth!=12):
+      currMnth+=1
+    else:
+      currMnth=1
+      currYr+=1
+
+  # update dict
+  for row in range(df.shape[0]):
+    msg = bank_statement['Particulars'][row]
+    
+    isloan = False
+    if(msg==msg):    # is str
+      string = re.split(r"[-/;,.\s]", msg)
+      
+      for word in string:
+        if(word in keywords):
+          isloan = True
+          break
+
+    if(isloan):
+      date = bank_statement['Date'][row]
+      string = date.split('-')
+      currYr = string[0]
+      currMnth = string[1]
+      currStr = currYr + "-" + (currMnth)
+      
+      if(bank_statement['Debit'][row]==bank_statement['Debit'][row]):
+        monthly_loan[currStr][0] += float(bank_statement['Debit'][row])
+      elif(df['Credit'][row]==bank_statement['Credit'][row]):
+        monthly_loan[currStr][1] += float(bank_statement['Credit'][row])
+    # print(monthly_loan)
+    
+  return monthly_loan
