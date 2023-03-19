a330cb42365b1acf7814ca05f47dc645659080e8
Arup20MA20010 <baralarup1430@gmail.com>
2023-03-19 15:38:26 +0530

    APIResponse

--- analyserModule/api/urls.py
+++ analyserModule/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('analyse/api/bank-analysis',views.bank_analysis),
-    path('analyse/api/BankStatementAnalysis',views.bank_statement_analysis),
-    path('',views.gross_summary),
-    path('',views.bank_name),
+    path('bank-analysis', views.bank_analysis),
+    path('bank-statement-analysis', views.bank_statement_analysis),
+    path('dashboard/gross-summary', views.gross_summary),
+    path('dashboard/<str:bankName>', views.bank_name),
 ]
--- analyserModule/api/views.py
+++ analyserModule/api/views.py
@@ -3,30 +3,75 @@ import pickle
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import render
+from .serializers import BankAnalyserSerializer, GrossSummarySerializer, ParticularBankSerializer
+
+
+class BankAnalysisResponseBody():
+    def __init__(self,
+                 volatilityScore,
+                 bankName,
+                 avgMonthlySpending=0,
+                 percentMonthlySpending=0,
+                 SourcesOfLargeCredit=[],
+                 SourcesOfLargeDebit=[],
+                 frequencyOfCreditPayments=0,
+                 frequencyOfDebitPayments=0,
+                 frequencyOfPayment=0,
+                 spendingToIncomeRatio=0,
+                 daysToSpend50Percent=0,
+                 daysToSpend80Percent=0,
+                 recurringPayment=0
+                 ):
+        self.volatilityScore = volatilityScore
+        self.bankName = bankName
+        self.avgMonthlySpending = avgMonthlySpending
+        self.percentMonthlySpending = percentMonthlySpending
+        self.SourcesOfLargeCredit = SourcesOfLargeCredit
+        self.SourcesOfLargeDebit = SourcesOfLargeDebit
+        self.frequencyOfCreditPayments = frequencyOfCreditPayments
+        self.frequencyOfDebitPayments = frequencyOfDebitPayments
+        self.frequencyOfPayment = frequencyOfPayment
+        self.spendingToIncomeRatio = spendingToIncomeRatio
+        self.daysToSpend50Percent = daysToSpend50Percent
+        self.daysToSpend80Percent = daysToSpend80Percent
+        self.recurringPayment = recurringPayment
+
+
+class GrossSummaryResponseBody():
+    def __init__(self, avgBalance):
+        self.avgBalance = avgBalance
+
+
+class ParticularBankDetails():
+    def __init__(self, bankName):
+        self.bankName = bankName
+
 
 @api_view(['GET'])
 def bank_analysis(request):
-    return_data = {
+    resObj = BankAnalysisResponseBody(33, "AXIS")
+    serializer = BankAnalyserSerializer(resObj)
+    return Response(serializer.data)
 
-    }
-    return Response(return_data)
 
+@api_view(['GET'])
 def bank_statement_analysis(request):
-    return_data = {
+    resObj = BankAnalysisResponseBody(54, "SBI")
+    serializer = BankAnalyserSerializer(resObj)
+    return Response(serializer.data)
 
-    }
-    return Response(return_data)
 
+@api_view(['GET'])
 def gross_summary(request):
-    return_data = {
-        
-    }
-    return Response(return_data)
+    resObj = GrossSummaryResponseBody(100)
+    serializer = GrossSummarySerializer(resObj)
+    return Response(serializer.data)
 
-def bank_name(request):
-    return_data = {
 
-    }
-    return Response(return_data)
+@api_view(['GET'])
+def bank_name(request, bankName):
+    resObj = ParticularBankDetails(bankName)
+    serializer = ParticularBankSerializer(resObj)
+    return Response(serializer.data)
 
 # Create your views here.
