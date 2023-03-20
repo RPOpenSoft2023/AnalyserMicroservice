acbd769abb76022cea55357ad11cb6ce708877cd
shobhi10058 <rkgupta462gkp@gmail.com>
2023-03-20 23:22:46 +0530

    added phone number in model

--- analyserModule/api/models.py
+++ analyserModule/api/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class monthWiseAnalytics(models.Model): 
     accountNumber = models.BigIntegerField()
+    phoneNumber = models.BigIntegerField()
     month = models.IntegerField()
     year = models.IntegerField()
-    analysis = models.TextField()
+    analysis = models.TextField() # analysis of current month
+    
