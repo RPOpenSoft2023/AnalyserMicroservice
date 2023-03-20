ab9fee385153606a44551f279b90df5155b30c28
shobhi10058 <rkgupta462gkp@gmail.com>
2023-03-20 23:32:20 +0530

    removed conflicts

--- analyserModule/api/views.py
+++ analyserModule/api/views.py
@@ -4,11 +4,6 @@ import json
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import render
-<<<<<<< HEAD
-from rest_framework import status
-from .models import monthWiseAnalytics
-from .serializers import MonthWiseAnalyticsSerializer
-=======
 from .Helpers import Sources, Loans
 import pandas as pd
 # class BankAnalysisResponseBody():
@@ -52,23 +47,10 @@ import pandas as pd
 # class ParticularBankDetails():
 #     def __init__(self, bankName):
 #         self.bankName = bankName
->>>>>>> b5d62d462a4e2851340ef081014a9f53fa3b608a
 
 
 @api_view(['GET'])
 def bank_analysis(request):
-<<<<<<< HEAD
-    analysis = monthWiseAnalytics.objects.all()
-    serializer = MonthWiseAnalyticsSerializer(analysis, many = True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def upload_statement(request):
-    file = request.data['file']
-    print(file)
-    
-    
-=======
     try:
         temp = (request.body.decode('utf-8'))
         temp = json.loads(temp)
@@ -144,4 +126,3 @@ def bank_name(request, bankName):
         return Response({"Error": e}, status=400)
 
 # Create your views here.
->>>>>>> b5d62d462a4e2851340ef081014a9f53fa3b608a
