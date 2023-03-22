d14490d417fc091266ed14649d6e20a66f582226
Arup20MA20010 <baralarup1430@gmail.com>
2023-03-23 02:13:16 +0530

    MonthlyFunctions

--- analyserModule/api/Helpers/PaymentStat.py
+++ analyserModule/api/Helpers/PaymentStat.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 from datetime import date
+from . import basicUtility as bu
 
 
-def Freq_of_Debit_Payment(start_date, end_date, bank_statement_var):
+def Total_Freq_of_Debit_Payment(start_date, end_date, bank_statement_var):
     bank_statement = bank_statement_var.copy()
     bank_statement['Debit'] = bank_statement['Debit'].replace(np.nan, 0)
     data_refined = bank_statement[(bank_statement['Date'] >= start_date) & (
@@ -12,6 +13,63 @@ def Freq_of_Debit_Payment(start_date, end_date, bank_statement_var):
     debit_col = data_refined['Debit']
     debit_freq = debit_col.value_counts()  # to get counts of all values
     return (num_rows-debit_freq[0])
+
+
+def Total_Freq_of_Credit_Payment(start_date, end_date, bank_statement_var):
+    bank_statement = bank_statement_var.copy()
+    bank_statement['Credit'] = bank_statement['Credit'].replace(np.nan, 0)
+    data_refined = bank_statement[(bank_statement['Date'] >= start_date) & (
+        bank_statement['Date'] <= end_date)]
+    num_rows = data_refined.shape[0]  # to count number of rows
+    debit_col = data_refined['Credit']
+    debit_freq = debit_col.value_counts()  # to get counts of all values
+    return (num_rows-debit_freq[0])
+
+
+def freqOfCreditPayment(start_month, start_year, end_month, end_year, bank_statement_var):
+    bank_statement = bank_statement_var.copy()
+    freqList = []
+    if end_year == start_year:
+        for month in range(start_month, end_month+1):
+            start_date = bu.getStartDateFromMonthYear(month, start_year)
+            end_date = bu.getEndDateFromMonthYear(month, start_year)
+            freqList.append({str(month): Total_Freq_of_Credit_Payment(
+                start_date, end_date, bank_statement)})
+    else:
+        for month in range(start_month, 13):
+            start_date = bu.getStartDateFromMonthYear(month, start_year)
+            end_date = bu.getEndDateFromMonthYear(month, start_year)
+            freqList.append({str(month): Total_Freq_of_Credit_Payment(
+                start_date, end_date, bank_statement)})
+        for month in range(1, end_month+1):
+            start_date = bu.getStartDateFromMonthYear(month, end_year)
+            end_date = bu.getEndDateFromMonthYear(month, end_year)
+            freqList.append({str(month): Total_Freq_of_Credit_Payment(
+                start_date, end_date, bank_statement)})
+    return freqList
+
+
+def freqOfDebitPayment(start_month, start_year, end_month, end_year, bank_statement_var):
+    bank_statement = bank_statement_var.copy()
+    freqList = []
+    if end_year == start_year:
+        for month in range(start_month, end_month+1):
+            start_date = bu.getStartDateFromMonthYear(month, start_year)
+            end_date = bu.getEndDateFromMonthYear(month, start_year)
+            freqList.append({str(month): Total_Freq_of_Debit_Payment(
+                start_date, end_date, bank_statement)})
+    else:
+        for month in range(start_month, 13):
+            start_date = bu.getStartDateFromMonthYear(month, start_year)
+            end_date = bu.getEndDateFromMonthYear(month, start_year)
+            freqList.append({str(month): Total_Freq_of_Debit_Payment(
+                start_date, end_date, bank_statement)})
+        for month in range(1, end_month+1):
+            start_date = bu.getStartDateFromMonthYear(month, end_year)
+            end_date = bu.getEndDateFromMonthYear(month, end_year)
+            freqList.append({str(month): Total_Freq_of_Debit_Payment(
+                start_date, end_date, bank_statement)})
+    return freqList
 
 
 def avg_monthly_expenditure(start_date, end_date, bank_statement):
