5e525c32017a02a381e221bd2a1f85b9803df15f
shobhi10058 <rkgupta462gkp@gmail.com>
2023-03-25 17:04:03 +0530

    added / at the end of url

--- analyserModule/api/urls.py
+++ analyserModule/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('bank-analysis', views.bank_analysis),
-    path('add-statement', views.bank_account_init),
-    path('statement-analyse', views.bank_statement_analyse),
-    path('edit-transaction', views.edit_transaction),
+    path('bank-analysis/', views.bank_analysis),
+    path('add-statement/', views.bank_account_init),
+    path('statement-analyse/', views.bank_statement_analyse),
+    path('edit-transaction/', views.edit_transaction),
     # TODO: add transaction
 ]
