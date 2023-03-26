4eaa237b90c96d777d7c4070aa72caaf5faf20d8
SnehalSwadhin <snehalswadhin@gmail.com>
2023-03-27 00:23:19 +0530

    Updated env-vars

--- env-vars.ps1
+++ env-vars.ps1
@@ -2,5 +2,5 @@
 [System.Environment]::SetEnvironmentVariable('DATABASE_PASSWORD', 'demo')
 [System.Environment]::SetEnvironmentVariable('DATABASE_NAME', 'analyser-db')
 
-[System.Environment]::SetEnvironmentVariable('USER_MICROSERVICE', 'http://34.105.83.175:80/user/api/')
-[System.Environment]::SetEnvironmentVariable('BANKING_MICROSERVICE', 'http://34.82.249.105:80/banking/api/')
+[System.Environment]::SetEnvironmentVariable('USER_MICROSERVICE', 'https://users-ms.apps.sandbox-m3.1530.p1.openshiftapps.com/user/api/')
+[System.Environment]::SetEnvironmentVariable('BANKING_MICROSERVICE', 'https://banking-ms.apps.sandbox-m3.1530.p1.openshiftapps.com/banking/api/')
